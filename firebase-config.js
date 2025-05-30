1b4393611acda3cf0913567915f3e61ab6dede02
TCreaMYPE <158609782+tcreamype3R@users.noreply.github.com>
2025-05-30 14:20:46 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,1 @@
-export const firebaseConfig = {
-  const firebaseConfig = {
-    apiKey: "AIzaSyC1Kla8uiumfInbR0ufL3X5OTeNDg7kBT0",
-    authDomain: "elarboldelafe.firebaseapp.com",
-    projectId: "elarboldelafe",
-    storageBucket: "elarboldelafe.firebasestorage.app",
-    messagingSenderId: "480772733343",
-    appId: "1:480772733343:web:b8f94238bbe4ffa7a4a5aa",
-  };
+
